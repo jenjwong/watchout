268211f90fb3bdac840f94c0a4b4b2ff5915533c
jenjwong <jenjwong@gmail.com>
2016-04-29 19:10:50 -0700

    gets player to move with mouse drag

--- client/watchout.js
+++ client/watchout.js
@@ -1,14 +1,15 @@
 // start slingin' some d3 here.
 
-
+// d3 selector for the svg-g
 var svg = d3.select('body').append('svg')
   .attr('height', 500)
   .attr('width', 500)
-.append('g')
-  .attr('height', 500 - 100)
-  .attr('width', 500 - 100)
-  .attr('transform', 'translate(' + 10 + ',' + 10 + ')' );
+.append('g');
+  // .attr('height', 500 - 100)
+  // .attr('width', 500 - 100);
+  // .attr('transform', 'translate(' + 10 + ',' + 10 + ')' );
 
+//makes n enemies with random positions
 var makeEnemies = function(n) {
   var arr = [];
   for (var i = 0; i < n; i++) {
@@ -23,6 +24,7 @@ var makeEnemies = function(n) {
 
 allEnemies = makeEnemies(10);
 
+// d3 selector for the circle enemies 
 var brdEnemies = svg.selectAll('enemies').data(allEnemies)
   .enter().append('svg:circle')
     .attr('class', 'enemy')
@@ -30,6 +32,7 @@ var brdEnemies = svg.selectAll('enemies').data(allEnemies)
     .attr('cy', function(d) { return d.y; } )
     .attr('r', 10);
 
+// function that generates random enemy positons
 var randMovEnemies = function(dataArray) {
   for (var i = 0; i < dataArray.length; i++) {
     dataArray[i].x = Math.random() * 400;
@@ -38,6 +41,8 @@ var randMovEnemies = function(dataArray) {
   return dataArray;
 };
 
+
+//d3 update function
 var update = function(dataArray) {
   //update
   brdEnemies.data(dataArray)
@@ -49,6 +54,40 @@ var update = function(dataArray) {
   //exit
 };
 
+
 setInterval(function() {
   update(randMovEnemies(allEnemies));
-}, 2000);
+}, 1000);
+
+
+var Player = function(x, y) {
+  var player = {};
+  player.id = 'player';
+  player.x = x;
+  player.y = y;
+
+  return player;
+};
+
+var players = [ Player(200, 200) ];
+
+var player = svg.selectAll('player').data(players)
+  .enter().append('svg:rect')
+    .attr('width', 10)
+    .attr('height', 10)
+    .attr('x', function(d) { return d.x; } )
+    .attr('y', function(d) { return d.y; } );
+
+var onDrag = function(d) {
+  var x = (d3.event.x);
+  var y = (d3.event.y);
+
+  d3.select(this).attr('x', d.x = x )
+    .attr('y', d.y = y );
+};
+
+var dragBehavior = d3.behavior.drag()
+    .on('drag', onDrag);
+
+player.call(dragBehavior);
+
